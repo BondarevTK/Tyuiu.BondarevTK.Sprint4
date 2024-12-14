d272f36f0da45648239ed774f45f988f03e5e227
BondarevTK <BondarevTK101@gmail.com>
2024-12-14 10:37:09 +0300

    Task3

---
+++ Tyuiu.BondarevTK.Sprint4.Task3.V9.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BondarevTK.Sprint4.Task3.V9.Lib
+{
+    public class DataService : ISprint4Task3V9
+    {
+        public int Calculate(int[,] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; i < array.GetLength(1); i++)
+                {
+                    if (array[i, j] == array[i,2])
+                    {
+                        sum += array[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.BondarevTK.Sprint4.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BondarevTK.Sprint4.Task3.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
