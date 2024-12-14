9c56d3839d8babb2e43f6739b56d5ca553381a48
BondarevTK <BondarevTK101@gmail.com>
2024-12-14 11:38:40 +0300

    t4

---
+++ Tyuiu.BondarevTK.Sprint4.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BondarevTK.Sprint4.Task4.V23.Lib
+{
+    public class DataService : ISprint4Task4V23
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] != 0)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+
+
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.BondarevTK.Sprint4.Task4.V23/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.BondarevTK.Sprint4.Task4.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
