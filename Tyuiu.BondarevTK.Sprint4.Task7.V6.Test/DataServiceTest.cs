27a98b3b6733548e079bf3c38cd544052544bd4b
BondarevTK <BondarevTK101@gmail.com>
2024-12-14 12:46:27 +0300

    T7

---
+++ Tyuiu.BondarevTK.Sprint4.Task7.V6.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BondarevTK.Sprint4.Task7.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BondarevTK.Sprint4.Task7.V6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.BondarevTK.Sprint4.Task7.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
