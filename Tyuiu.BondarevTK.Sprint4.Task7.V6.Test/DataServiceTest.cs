e5639afadf2cd602a24e441c424659d95809e47f
BondarevTK <BondarevTK101@gmail.com>
2024-12-14 12:49:54 +0300

    t7

--- Tyuiu.BondarevTK.Sprint4.Task7.V6.Test/DataServiceTest.cs
+++ Tyuiu.BondarevTK.Sprint4.Task7.V6.Test/DataServiceTest.cs
@@ -1,3 +1,4 @@
+using Tyuiu.BondarevTK.Sprint4.Task7.V6.Lib;
 namespace Tyuiu.BondarevTK.Sprint4.Task7.V6.Test
 {
     [TestClass]
@@ -6,6 +7,12 @@ namespace Tyuiu.BondarevTK.Sprint4.Task7.V6.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            string value = "458963214789";
+            int n = 3;
+            int m = 4;
+            int res = ds.Calculate(n, m, value);
+            Assert.AreEqual(34, res);
         }
     }
 }
--- Tyuiu.BondarevTK.Sprint4.Task7.V6/Program.cs
+++ Tyuiu.BondarevTK.Sprint4.Task7.V6/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Tyuiu.BondarevTK.Sprint4.Task7.V6
+﻿using Tyuiu.BondarevTK.Sprint4.Task7.V6.Lib;
+namespace Tyuiu.BondarevTK.Sprint4.Task7.V6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            string value = "458963214789";
+            int n = 3;
+            int m = 4;
+            int res = ds.Calculate(n, m, value);
+            Console.WriteLine(res);
         }
     }
 }
