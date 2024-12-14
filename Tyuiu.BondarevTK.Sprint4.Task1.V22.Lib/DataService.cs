9b478e2dc222e88d557e3dd8d68e7496b3a9b650
BondarevTK <BondarevTK101@gmail.com>
2024-12-14 09:39:00 +0300

    t1

--- Tyuiu.BondarevTK.Sprint4.Task1.V22.Lib/DataService.cs
+++ Tyuiu.BondarevTK.Sprint4.Task1.V22.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.BondarevTK.Sprint4.Task1.V22.Lib
             int sum = 1;
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] !=0)
+                if (array[i] % 2 !=0)
                 {
                     sum *= array[i];
                 }
--- Tyuiu.BondarevTK.Sprint4.Task1.V22.Test/DataServiceTest.cs
+++ Tyuiu.BondarevTK.Sprint4.Task1.V22.Test/DataServiceTest.cs
@@ -8,9 +8,9 @@ namespace Tyuiu.BondarevTK.Sprint4.Task1.V22.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            int[] array = new int[] { 2, 2};
+            int[] array = new int[] { 3, 3};
             int res = ds.Calculate(array);
-            Assert.AreEqual(4, res);
+            Assert.AreEqual(9, res);
         }
     }
 }
