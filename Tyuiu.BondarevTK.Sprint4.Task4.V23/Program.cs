75414a4e9921add0f71c8262bc3fd40036ae3dcc
BondarevTK <BondarevTK101@gmail.com>
2024-12-14 11:44:02 +0300

    t4

--- Tyuiu.BondarevTK.Sprint4.Task4.V23/Program.cs
+++ Tyuiu.BondarevTK.Sprint4.Task4.V23/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Tyuiu.BondarevTK.Sprint4.Task4.V23
+﻿using Tyuiu.BondarevTK.Sprint4.Task4.V23.Lib;
+namespace Tyuiu.BondarevTK.Sprint4.Task4.V23
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[,] matrix = new int[5, 5];
+            for (int i = 0;  i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    matrix[i, j] = x;
+                }
+            }
+            DataService ds = new DataService();
+            int res = ds.Calculate(matrix);
+            Console.WriteLine(res);
         }
     }
 }
